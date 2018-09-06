773b3b1f4823381bb13e29492f099735920428a4
Lord Vega <averydante9@gmail.com>
2018-09-06 02:12:51 -0400

    added updated js game logic file

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardThree);  
+ 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []; 
+var cardOne = cards[3];
+cardsInPlay.push(cardOne);
+console.log( "user flipped queen");
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("user flipped king");
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!!");
+	} else {
+		alert("Sorry, try again");
+	}
+}
