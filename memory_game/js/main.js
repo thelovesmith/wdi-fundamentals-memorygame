3c0a8844c633d326f8bc6cb15d7a3d74e434e384
Lord Vega <averydante9@gmail.com>
2018-09-06 03:06:52 -0400

    updated game logic with js f(x)

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,19 @@
  
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = []; 
-var cardOne = cards[3];
-cardsInPlay.push(cardOne);
-console.log( "user flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("user flipped king");
-if (cardsInPlay.length === 2){
+var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!!");
-	} else {
-		alert("Sorry, try again");
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+}
+}
+var flipCard = function(cardId){
+	console.log("User flipped" + " " + cards[cardId]);
+	cardsInPlay.push(cards[cardId])
+	if (cardsInPlay.length === 2){
+		checkForMatch()
 	}
 }
+flipCard(0)
+flipCard(2)
